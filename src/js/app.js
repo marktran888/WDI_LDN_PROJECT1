d17d8d57f36742f98c68f78f097671aa5dd159bb
Mark Tran <mark.tran@hotmail.co.uk>
2018-02-09 13:11:41 +0000

    added variables and addToSnakeBody function

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,25 @@
 
-[10,20,30].forEach(num => console.log(num));
+const column = 10;
+const width = 10;
+let snakeBody = [];
+let snakeHead;
+let snakeTail;
+const directions = ['N','E','S','W'];
+const direction = directions[Math.floor(Math.random()*directions.length)];
+let startPoint = Math.floor(Math.random() * column * width);
+
+
+
+function addToSnakeBody(item){
+  return snakeBody.push(item);
+}
+
+function init() {
+      // console.log(startPoint);
+  snakeHead = startPoint;
+  addToSnakeBody(snakeHead);
+      // console.log(snakeBody);
+      console.log(direction);
+
+}
+window.addEventListener('DOMContentLoaded', init);
