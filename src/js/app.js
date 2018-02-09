110ad2fde5ae7215c25b759c6b0454914bb3b838
Mark Tran <mark.tran@hotmail.co.uk>
2018-02-09 14:31:49 +0000

    added build grid

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,3 @@
-
 const column = 10;
 const width = 10;
 let snakeBody = [];
@@ -8,18 +7,36 @@ const directions = ['N','E','S','W'];
 const direction = directions[Math.floor(Math.random()*directions.length)];
 let startPoint = Math.floor(Math.random() * column * width);
 
-
-
 function addToSnakeBody(item){
   return snakeBody.push(item);
 }
 
 function init() {
-      // console.log(startPoint);
+
+  //Build grid
+  let cellNumber = 0;
+  const gridElement = document.createElement('div');
+  document.body.appendChild(gridElement);
+  gridElement.classList.add('grid');
+
+  for (let i = 0; i < column; i++) {
+    const rowElement = document.createElement('div');
+    gridElement.appendChild(rowElement);
+    rowElement.classList.add('row');
+
+    for (let j = 0; j < width; j++) {
+      const cellElement = document.createElement('div');
+      rowElement.appendChild(cellElement);
+      cellElement.classList.add('cell');
+      cellElement.classList.add(cellNumber);
+      cellNumber++;
+    }
+  }
+
+
   snakeHead = startPoint;
   addToSnakeBody(snakeHead);
-      // console.log(snakeBody);
-      console.log(direction);
-
 }
+
+
 window.addEventListener('DOMContentLoaded', init);
